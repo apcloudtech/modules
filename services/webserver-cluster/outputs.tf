fded0be007303dd8996ddd8f95d5ca3aad2a8e81
apcloudtech <apcloudtech@gmail.com>
2022-07-15 15:23:38 -0700

    duplicate

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -8,7 +8,7 @@ output "asg_name" {
   value       = aws_autoscaling_group.example.name
   description = "The name of the Auto Scaling Group"
 }
-output "clb_dns_namee" {
+output "clb_dns_name" {
   value       = aws_elb.web_server_elb.dns_name
   description = "The domain name of the load balancer"
 }
